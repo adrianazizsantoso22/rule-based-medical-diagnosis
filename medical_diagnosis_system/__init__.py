9d05985b96049d118ed34313854b87abe9e6da49
adrianazizsantoso22 <adrian22pass@gmail.com>
2024-12-14 16:40:34 +0700

    Create __init__.py

---
+++ medical_diagnosis_system/__init__.py
@@ -0,0 +1,5 @@
+# medical_diagnosis_system/__init__.py
+
+from .diagnosis import diagnosis  # Import the diagnosis function
+
+__all__ = ['diagnosis']  # Specifies what is imported when 'from medical_diagnosis_system import *' is used
