bf9721a024e69fe45c6b84dda958885b9920e5cd
adrianazizsantoso22 <adrian22pass@gmail.com>
2024-12-15 14:56:52 +0700

    Update __init__.py

--- medical_diagnosis_system/__init__.py
+++ medical_diagnosis_system/__init__.py
@@ -1,5 +1,5 @@
 # medical_diagnosis_system/__init__.py
 
-from .diagnosis import diagnosis  # Import the diagnosis function
+from diagnosis import diagnosis  # Import the diagnosis function
 
 __all__ = ['diagnosis']  # Restrict wildcard imports to include only 'diagnosis'
